b29be84f9289cf59255e0a2e7b3d9d8a0a520d26
Aaronchen01 <Aaronchen99@163.com>
2018-04-12 12:53:43 +0800

    add vue connect and chart

--- src/main/java/com/sales/controller/SalesController.java
+++ src/main/java/com/sales/controller/SalesController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class SalesController {
     private GoodsRepository goodsRepository;
 
     @GetMapping(value = "/goods")
+    @ResponseBody
     public List<Goods> goodsList(){
         return goodsRepository.findAll();
     }
